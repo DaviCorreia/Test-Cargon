15200dbbcb4ff86ce59cd44062982a214dc737a5
Davi Correia <davi.correia@dcx.ufpb.br>
2024-07-06 20:01:43 -0300

    creation of context and filters and folder with icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
-import Image from "next/image";
+"use client";
+
 import styles from "./page.module.css";
+import { FilterBar } from "./components/filter-bar";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-          
-      </div>
+      <FilterBar/>
     </main>
   );
 }
